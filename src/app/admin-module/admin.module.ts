cc8d94f9bb6604a7005dff43fcddf11377cd37a2
MelbexDeLeon <melbex11@gmail.com>
2018-04-21 20:53:15 +0800

    updated layer list design

--- src/app/admin-module/admin.module.ts
+++ src/app/admin-module/admin.module.ts
@@ -5,16 +5,23 @@ import { RouterLinkActive } from "@angular/router";
 import { DashboardComponent } from "./dashboard/dashboard.component";
 import { LayerlistComponent } from "./layerlist/layerlist.component";
 import { UploadLayerComponent } from "./upload-layer/upload-layer.component";
+import { MapListComponent } from "./map-list/map-list.component"; 
 
 import { AdminRoutingModule } from "./admin-routing.module";
 
 import { MatTabsModule } from "@angular/material/tabs";
+/* prepare to remove this modules */
 import { MatListModule } from "@angular/material/list";
 import { MatDividerModule } from "@angular/material/divider";
-import { MapListComponent } from './map-list/map-list.component';
+/*  */
 import { MatIconModule } from "@angular/material/icon";
 import { MatButtonModule } from "@angular/material/button";
 import { MatTooltipModule } from "@angular/material/tooltip";
+import { MatTableModule } from "@angular/material/table";
+import { MatPaginatorModule } from "@angular/material/paginator";
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from "@angular/material/input";
+import { MatCheckboxModule } from "@angular/material/checkbox";
 
 import { MapService } from "../services/map.service";
 import { LayerService } from "../services/layer.service";
@@ -27,7 +34,12 @@ import { LayerService } from "../services/layer.service";
     MatListModule,
     MatIconModule,
     MatButtonModule,
-    MatTooltipModule
+    MatTooltipModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCheckboxModule
   ],
   declarations: [
     DashboardComponent,
--- src/app/admin-module/layerlist/layerlist.component.ts
+++ src/app/admin-module/layerlist/layerlist.component.ts
@@ -1,34 +1,119 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
 import { LayerService } from "../../services/layer.service";
 import { saveAs as SaveAs} from "file-saver";
+import { MatPaginator, MatTableDataSource } from "@angular/material";
+import { SelectionModel } from "@angular/cdk/collections";
 
 @Component({
-  selector: 'app-layer-list',
-  templateUrl: './layerlist.component.html',
-  styleUrls: ['./layerlist.component.css']
+  selector: "app-layer-list",
+  templateUrl: "./layerlist.component.html",
+  styleUrls: ["./layerlist.component.css"]
 })
-export class LayerlistComponent implements OnInit {
+export class LayerlistComponent implements OnInit, AfterViewInit {
   private dummyData: any;
-  position = "above"
+  private columnDef: any = [];
+  private dataSource: any;
+  selection = new SelectionModel<Element>(true, []);
+  position = "above";
 
-  constructor(private layerService: LayerService) { 
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  constructor(private layerService: LayerService) {
     this.dummyData = [
-      { id: "1", name: "Aborlan.geojson", type: "L.GeoJSON", link: "https://www.googleapis.com/storage/v1/b/modecera-geojson-files/o/Aborlan.geojson?alt=media" }, 
-      { id: "2", name: "Balogo.geojson", type: "L.GeoJSON", link: "https://www.googleapis.com/storage/v1/b/modecera-geojson-files/o/Balogo.geojson?alt=media" },
-      { id: "3", name: "Carranglan.geojson", type: "L.GeoJSON", link: "https://www.googleapis.com/storage/v1/b/modecera-geojson-files/o/Carranglan.geojson?alt=media"},
-      { id: "4", name: "Aborlan.geojson", type: "L.GeoJSON", link: "https://www.googleapis.com/storage/v1/b/modecera-geojson-files/o/Aborlan.geojson?alt=media" },
-      { id: "1", name: "Aborlan.geojson", type: "L.GeoJSON", link: "https://www.googleapis.com/storage/v1/b/modecera-geojson-files/o/Aborlan.geojson?alt=media" }, 
-      { id: "2", name: "Balogo.geojson", type: "L.GeoJSON", link: "https://www.googleapis.com/storage/v1/b/modecera-geojson-files/o/Balogo.geojson?alt=media" },
-      { id: "3", name: "Carranglan.geojson", type: "L.GeoJSON", link: "https://www.googleapis.com/storage/v1/b/modecera-geojson-files/o/Carranglan.geojson?alt=media"},
-      { id: "4", name: "Aborlan.geojson", type: "L.GeoJSON", link: "https://www.googleapis.com/storage/v1/b/modecera-geojson-files/o/Aborlan.geojson?alt=media" },
-      { id: "1", name: "Aborlan.geojson", type: "L.GeoJSON", link: "https://www.googleapis.com/storage/v1/b/modecera-geojson-files/o/Aborlan.geojson?alt=media" }, 
-      { id: "2", name: "Balogo.geojson", type: "L.GeoJSON", link: "https://www.googleapis.com/storage/v1/b/modecera-geojson-files/o/Balogo.geojson?alt=media" },
-      { id: "3", name: "Carranglan.geojson", type: "L.GeoJSON", link: "https://www.googleapis.com/storage/v1/b/modecera-geojson-files/o/Carranglan.geojson?alt=media"},
-      { id: "4", name: "Aborlan.geojson", type: "L.GeoJSON", link: "https://www.googleapis.com/storage/v1/b/modecera-geojson-files/o/Aborlan.geojson?alt=media" },
+      {
+        id: "1",
+        name: "Aborlan.geojson",
+        type: "L.GeoJSON",
+        link:
+          "https://www.googleapis.com/storage/v1/b/modecera-geojson-files/o/Aborlan.geojson?alt=media"
+      },
+      {
+        id: "2",
+        name: "Balogo.geojson",
+        type: "L.GeoJSON",
+        link:
+          "https://www.googleapis.com/storage/v1/b/modecera-geojson-files/o/Balogo.geojson?alt=media"
+      },
+      {
+        id: "3",
+        name: "Carranglan.geojson",
+        type: "L.GeoJSON",
+        link:
+          "https://www.googleapis.com/storage/v1/b/modecera-geojson-files/o/Carranglan.geojson?alt=media"
+      },
+      {
+        id: "4",
+        name: "Aborlan.geojson",
+        type: "L.GeoJSON",
+        link:
+          "https://www.googleapis.com/storage/v1/b/modecera-geojson-files/o/Aborlan.geojson?alt=media"
+      },
+      {
+        id: "1",
+        name: "Aborlan.geojson",
+        type: "L.GeoJSON",
+        link:
+          "https://www.googleapis.com/storage/v1/b/modecera-geojson-files/o/Aborlan.geojson?alt=media"
+      },
+      {
+        id: "2",
+        name: "Balogo.geojson",
+        type: "L.GeoJSON",
+        link:
+          "https://www.googleapis.com/storage/v1/b/modecera-geojson-files/o/Balogo.geojson?alt=media"
+      },
+      {
+        id: "3",
+        name: "Carranglan.geojson",
+        type: "L.GeoJSON",
+        link:
+          "https://www.googleapis.com/storage/v1/b/modecera-geojson-files/o/Carranglan.geojson?alt=media"
+      },
+      {
+        id: "4",
+        name: "Aborlan.geojson",
+        type: "L.GeoJSON",
+        link:
+          "https://www.googleapis.com/storage/v1/b/modecera-geojson-files/o/Aborlan.geojson?alt=media"
+      },
+      {
+        id: "1",
+        name: "Aborlan.geojson",
+        type: "L.GeoJSON",
+        link:
+          "https://www.googleapis.com/storage/v1/b/modecera-geojson-files/o/Aborlan.geojson?alt=media"
+      },
+      {
+        id: "2",
+        name: "Balogo.geojson",
+        type: "L.GeoJSON",
+        link:
+          "https://www.googleapis.com/storage/v1/b/modecera-geojson-files/o/Balogo.geojson?alt=media"
+      },
+      {
+        id: "3",
+        name: "Carranglan.geojson",
+        type: "L.GeoJSON",
+        link:
+          "https://www.googleapis.com/storage/v1/b/modecera-geojson-files/o/Carranglan.geojson?alt=media"
+      },
+      {
+        id: "4",
+        name: "Aborlan.geojson",
+        type: "L.GeoJSON",
+        link:
+          "https://www.googleapis.com/storage/v1/b/modecera-geojson-files/o/Aborlan.geojson?alt=media"
+      }
     ];
+
+    this.columnDef = ["select", "name", "type", "link"];
+    this.dataSource = new MatTableDataSource<Element>(this.dummyData);
   }
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
   }
 
   downloadHandler(link: string, name: string): void {
@@ -36,5 +121,23 @@ export class LayerlistComponent implements OnInit {
       console.log(blob);
       SaveAs(blob, name);
     });
+  }
+
+  applyFilter(filterValue: string) {
+    filterValue = filterValue.trim(); // Remove whitespace
+    filterValue = filterValue.toLowerCase(); // MatTableDataSource defaults to lowercase matches
+    this.dataSource.filter = filterValue;
+  }
+
+  masterToggle() {
+    this.isAllSelected()
+      ? this.selection.clear()
+      : this.dataSource.data.forEach(row => this.selection.select(row));
+  }
+
+  isAllSelected() {
+    const numSelected = this.selection.selected.length;
+    const numRows = this.dataSource.data.length;
+    return numSelected === numRows;
   }
 }
